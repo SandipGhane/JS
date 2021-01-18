7ef7d916d4eddc58411b19d3d9f19bf034aec3aa
SandipGhane <sandip.humation@gmail.com>
2021-01-18 10:13:40 +0530

    docs : feat scope in js

---
+++ HardParts Functional Programing/blockScope.js
@@ -0,0 +1,11 @@
+scope();
+function scope(){
+    if(true){
+        const first = 'sandip';
+        let lname = 'Ghane';
+        var middle = 'Rangnath';
+    }
+    console.log(lname);
+    // console.log(first);
+    console.log(middle);
+}
