fd8c40de65ea0e8934b2aba7edae674602fdf5b0
SandipGhane <sandip.humation@gmail.com>
2021-01-24 14:55:46 +0530

    feat : some examples

--- ES6/destructring.js
+++ ES6/destructring.js
@@ -27,14 +27,43 @@
 
 
 //find nonDuplicates
-let arr = [1, 2, 3, 4.4, 2];
+// let arr = [1, 2, 3, 4.4, 2];
 
-//using filter
+// //using filter
 
-function findNonDuplicates (array){
-    return array.filter((elem,index)=>{
-        return array.indexOf(elem) === index;
-    })
+// function findNonDuplicates (array){
+//     return array.filter((elem,index)=>{
+//         return array.indexOf(elem) === index;
+//     })
+// }
+
+// console.log(findNonDuplicates(arr));
+
+// var persons = [
+//     {id : 1, name : "John", tags : "javascript"}, 
+//     {id : 2, name : "Alice", tags : "javascript"}, 
+//     {id : 3, name : "Roger", tags : "java"},
+//     {id : 4, name : "Adam", tags : "javascript"},
+//     {id : 5, name : "Alex", tags : "java"}
+//   ];
+  
+//   var uniqueTags = persons.reduce((acc, personObj) => {
+//       console.log('acc',acc);
+//       console.log('personObj',personObj);
+//       acc[personObj.tags] = 1;
+//       return acc;
+//   },{});
+
+//   console.log(uniqueTags);
+
+var arr1 = ["apple","orange","apple","orange","pear","orange"];
+
+function getWordCount(arr){
+    let result = {};
+    for(let i=0;i<arr.length;i++){
+        item = arr[i];
+        result[item] = (result[item] + 1)||1;
+    }
+    return result;
 }
-
-console.log(findNonDuplicates(arr));
+console.log(getWordCount(arr1));
