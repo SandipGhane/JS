528ceb035464ace72c03b27701621526ac8d8c17
SandipGhane <sandip.humation@gmail.com>
2021-02-10 16:11:57 +0530

    feat : some method in array

--- ES6_Methods/newArray.js
+++ ES6_Methods/newArray.js
@@ -1,4 +1,4 @@
-// 1.Array.isArray()
+// 1.Array.isArray() - boolean result
 // The Array.isArray() method is used to determine true/false if a value is a true array.
 // This method is check to see if the provided value is an instance of Array constructor
 
@@ -9,3 +9,14 @@ console.log(Array.isArray([1, 2, 3])); //logs true
 
 console.log(Array.isArray({ length: 3, 0: 1, 1: 2, 2: 3 }));
 console.log(typeof { length: 3, 0: 1, 1: 2, 2: 3 });
+
+
+// 2. [].some() - boolean result
+// This method will start testing values in array until a test return true.
+// then the function pass to the some and return immediately true ow return false.
+// if the first truthy value found will result in the function immediately returming true and this could mean that not test all values.
+
+var result = [2, 4, 6].some((value) => {
+    return value >= 4;
+})
+console.log('some-', result);
