5c16a47a8164fdb8f58d10c18e6d5da96c8ab7c3
SandipGhane <sandip.humation@gmail.com>
2021-02-10 17:26:31 +0530

    fear : map/reduce

--- ES6_Methods/newArray.js
+++ ES6_Methods/newArray.js
@@ -7,8 +7,8 @@ console.log(Array.isArray([1, 2, 3])); //logs true
 
 // Note - does not eork on like array of object
 
-console.log(Array.isArray({ length: 3, 0: 1, 1: 2, 2: 3 }));
-console.log(typeof { length: 3, 0: 1, 1: 2, 2: 3 });
+console.log(Array.isArray({ length: 3, 0: 1, 1: 2, 2: 3 })); //false
+console.log(typeof { length: 3, 0: 1, 1: 2, 2: 3 }); //object
 
 
 // 2. [].some() - boolean result
@@ -19,7 +19,7 @@ console.log(typeof { length: 3, 0: 1, 1: 2, 2: 3 });
 var Someresult = [2, 4, 6].some((value) => {
     return value >= 4;
 })
-console.log('Some-', Someresult);
+console.log('Some-', Someresult); //true
 
 // 3. [].every() - boolean result;
 // The [].every() method will start testing values in array until test result return false.
@@ -29,7 +29,7 @@ console.log('Some-', Someresult);
 var everyResult = [2, 4, 6, 8, 10].every((value) => {
     return value % 2 !== 0;
 })
-console.log('Every-', everyResult);
+console.log('Every-', everyResult); //true
 
 // 4. [].filter() - new Array
 // This method will return a new array containing all values that pass through filter test.
@@ -37,17 +37,44 @@ console.log('Every-', everyResult);
 var filterArray = [1, 2, 3, 4, 5, 6, 7, 8].filter((value) => {
     return value % 2 === 0
 })
-console.log('Filter -', filterArray);
+console.log('Filter -', filterArray); //logs [2,4,6,8]
 
 // 4.forEach()
 // This method executes a provided function for each value in array.
 
 var forE = [1, 2, 3, 4, 5].forEach((value) => {
-    console.log(value * 2);
+    console.log(value * 2); //logs 2,4,6,8,10
 })
 // console.log('ForEach -', forE);
 
 // 5. indexOf()
 // This method search provided value in whole array and returns index of this first found value.
 // if not found returns -1
-console.log([1, 2, 3, 4, 5].indexOf(1));
+console.log([1, 2, 3, 4, 5].indexOf(1)); //logs 0
+
+// 6.lastIndexOf()
+// The [].lastIndexOf() method searches an array for the last value matching the value passed to [].lastIndexOf(), and returns the index of this value.
+console.log([1, 2, 3, 4, 5, 1].lastIndexOf(1)); //logs 5
+
+// 7. map()
+// This method executes a provided function for each value in array and return a result in newArray.
+
+var mapArray = [1, 2, 3, 4, 54, 5].map((value) => {
+    return value * 2;
+})
+console.log('Map - ', mapArray);
+
+// 8.Reduce()
+// The reduce method runs a function that passes the return value to the next iteration of the function using values in array from left to right and returning final value
+
+// var reduceArrayValue = [1, 2, 3, 4, 5, 6].reduce((returningValueEachItr, value) => {
+//     return returningValueEachItr + value;
+// })
+// console.log('Reduce- ', reduceArrayValue);//logs 21
+
+var reduceArrayValue = [1, 2, 3, 4, 5, 6].reduce((returningValueEachItr, value) => {
+    return returningValueEachItr + value;
+}, 10)
+console.log('Reduce-Passing-default', reduceArrayValue);
+
+// we have also passed default value for the returningValueEachItr
